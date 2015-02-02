f80846f26912c1576ac66f962ee393961eda2ca9
Arthur Gautier <baloo@gandi.net>
2015-02-02 18:46:20 +0100

    Adds debug route
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- pyramidzipkin/__init__.py
+++ pyramidzipkin/__init__.py
@@ -2,7 +2,7 @@ from pyramid.config import Configurator
 
 #from sqlalchemy import engine_from_config
 
-from .views import root
+from .views import root, sleep_for
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -11,6 +11,8 @@ def main(global_config, **settings):
     config.include('zipkin_pyramid')
 
     config.add_route('root', '/')
+    config.add_route('sleep', '/sleep/{time}')
     config.add_view(root, route_name='root')
+    config.add_view(sleep_for, route_name='sleep')
 
     return config.make_wsgi_app()
--- pyramidzipkin/views.py
+++ pyramidzipkin/views.py
@@ -1,9 +1,11 @@
+import time
+
 from pyramid.view import view_config
 from pyramid.response import Response
 
 from zipkin_pyramid import trace
 
-import time
+import requests
 
 
 def root(request):
@@ -12,10 +14,21 @@ def root(request):
     sleep()
     return Response('hello zipkin, %s' % str(headers))
 
+def sleep_for(request):
+    t = int(request.matchdict['time'])
+    if t >= 1:
+        print "sleeping 1"
+        sleep(1)
+        print "http://localhost:6543/sleep/%d" % (t - 1)
+        ret = requests.get("http://localhost:6543/sleep/%d" % (t - 1))
+        return Response(ret.text + "<br/>\nwaited 1sec")
+    else:
+        return Response("returned immediatly")
+
 @trace("subfunc")
 def subfunc():
     time.sleep(0.2)
 
 @trace
-def sleep():
-    time.sleep(0.4)
+def sleep(t = 0.4):
+    time.sleep(t)
