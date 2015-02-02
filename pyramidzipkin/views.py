5f212ce762b178e4f249087931d9d56e21f5b97c
Arthur Gautier <baloo@gandi.net>
2015-02-02 17:31:39 +0100

    Use of tracing decorator
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- pyramidzipkin/views.py
+++ pyramidzipkin/views.py
@@ -1,9 +1,21 @@
 from pyramid.view import view_config
 from pyramid.response import Response
-#from pyramid_zipkin._requests import requests
+
+from zipkin_pyramid import trace
+
+import time
 
 
 def root(request):
-    #headers = requests(request, 'delay', endpoint='httpbin').get('http://httpbin.org/delay/1').json()
     headers = {}
+    subfunc()
+    sleep()
     return Response('hello zipkin, %s' % str(headers))
+
+@trace("subfunc")
+def subfunc():
+    time.sleep(0.2)
+
+@trace
+def sleep():
+    time.sleep(0.4)
