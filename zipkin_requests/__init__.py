85bbe1fcb340dfb4dc0c2159ce909c779621bdbf
Arthur Gautier <baloo@gandi.net>
2015-02-04 08:50:27 +0100

    Support for requests == 0.14
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- zipkin_requests/__init__.py
+++ zipkin_requests/__init__.py
@@ -1,43 +1,65 @@
-from requests.adapters import HTTPAdapter
-import requests.sessions
-from zipkin import local
 
+import requests.sessions
+
+from zipkin import local
 from zipkin.models import Annotation
 from zipkin.util import hex_str
 
-class ZipkinHTTPAdapter(HTTPAdapter):
-    def __init__(self, trace, *args, **kwargs):
-        super(ZipkinHTTPAdapter, self).__init__(**kwargs)
-        self.parent_trace = trace
 
-    def send(self, request, stream=False, timeout=None, verify=True, cert=None, proxies=None):
-        request.trace = self.parent_trace.child("requests:%s %s" % (request.method, request.url))
-        forwarded_trace = request.trace.child_noref("subservice")
+def pre_request(request):
+    parent_trace = local().current
+    request.trace = parent_trace.child("requests:%s %s" % (request.method, request.url))
+    forwarded_trace = request.trace.child_noref("subservice")
 
-        request.headers['X-B3-TraceId'] = hex_str(forwarded_trace.trace_id)
-        request.headers['X-B3-SpanId'] = hex_str(forwarded_trace.span_id)
-        if forwarded_trace.parent_span_id is not None:
-            request.headers['X-B3-ParentSpanId'] = hex_str(forwarded_trace.parent_span_id)
+    request.headers['X-B3-TraceId'] = hex_str(forwarded_trace.trace_id)
+    request.headers['X-B3-SpanId'] = hex_str(forwarded_trace.span_id)
+    if forwarded_trace.parent_span_id is not None:
+        request.headers['X-B3-ParentSpanId'] = hex_str(forwarded_trace.parent_span_id)
 
-        request.trace.record(Annotation.string('http.uri', request.url))
-        request.trace.record(Annotation.server_recv())
+    request.trace.record(Annotation.string('http.uri', request.url))
+    request.trace.record(Annotation.server_recv())
 
-        return super(ZipkinHTTPAdapter, self).send(request, stream, timeout, verify, cert, proxies)
+    return request
 
-    def build_response(self, req, resp):
-        req.trace.record(Annotation.string('http.responsecode', '{0}'.format(getattr(resp, 'status', None))))
-        req.trace.record(Annotation.server_send())
 
-        return super(ZipkinHTTPAdapter, self).build_response(req, resp)
+def pre_response(resp, req=None):
+    if not req:
+        req = resp.request
 
+    req.trace.record(Annotation.string('http.responsecode', '{0}'.format(getattr(resp, 'status', None))))
+    req.trace.record(Annotation.server_send())
+
+    return resp
+
+
+try:
+    from requests.adapters import HTTPAdapter
+
+    class ZipkinHTTPAdapter(HTTPAdapter):
+        def send(self, request, stream=False, timeout=None, verify=True, cert=None, proxies=None):
+            pre_request(request)
+            return super(ZipkinHTTPAdapter, self).send(request, stream, timeout, verify, cert, proxies)
+
+        def build_response(self, req, resp):
+            pre_response(resp, req)
+
+            return super(ZipkinHTTPAdapter, self).build_response(req, resp)
+
+except ImportError:
+    # requests < 1.0.0
+    pass
 
 def _func(init):
     def func(self, *args, **kwargs):
         init(self, *args, **kwargs)
-        trace = local().current
-        self.mount('http://', ZipkinHTTPAdapter(trace))
-        self.mount('https://', ZipkinHTTPAdapter(trace))
+        if hasattr(self, 'mount'):
+            self.mount('http://', ZipkinHTTPAdapter())
+            self.mount('https://', ZipkinHTTPAdapter())
+        else:
+            self.hooks['pre_request'] = pre_request
+            self.hooks['response'] = pre_response
     return func
+
 
 def init():
     old_init = requests.sessions.Session.__init__
