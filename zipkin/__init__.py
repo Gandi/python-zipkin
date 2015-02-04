bf8907be717f77d83df8f25c3033ea51c27d1bc1
Guillaume Gauvrit <guillaume@gauvr.it>
2015-02-04 21:50:58 +0100

    Moving some configuration from pyramid_zipkin to zipkin

--- zipkin/__init__.py
+++ zipkin/__init__.py
@@ -3,4 +3,5 @@ __version__ = '0.1'
 
 # export the API here
 
+from .config import configure
 from .threading import local
--- zipkin_pyramid/__init__.py
+++ zipkin_pyramid/__init__.py
@@ -1,29 +1,3 @@
-import socket
 
-from pyramid.events import NewRequest
-from zipkin.models import Endpoint
-from pyramid.tweens import MAIN, EXCVIEW
-
-from .pyramidhook import wrap_request
-
-from .client import Client
+from .config import includeme
 from .api import *
-
-
-from zipkin_requests import init as request_init
-
-def includeme(config):
-    """Include the zipkin definitions"""
-
-    settings = config.registry.settings
-    if 'zipkin.collector' not in settings:
-        return
-    name = config.registry.__name__
-
-    ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
-    endpoint = Endpoint(ip, 0, name)
-    Client.configure(settings)
-
-    request_init()
-
-    config.add_subscriber(wrap_request(endpoint), NewRequest)
