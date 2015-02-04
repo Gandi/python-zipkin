6aaab69aef824a962d42f9d549f2011db82668aa
Arthur Gautier <baloo@gandi.net>
2015-02-04 20:08:47 +0100

    WIP
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- tasks.py
+++ tasks.py
@@ -1,17 +1,25 @@
+import time
+
 from celery import Celery
 from celery.signals import before_task_publish, task_prerun, task_postrun
 
 from zipkin_celery import zipkin_init
 from zipkin_pyramid.client import Client
 
-if __name__ == 'main':
-    Client.configure({'zipkin.collector': '127.0.0.1'})
-    zipkin_init()
+from zipkin_pyramid import trace
+
+#if __name__ == 'main':
+#    Client.configure({'zipkin.collector': '127.0.0.1'})
+zipkin_init()
 
 app = Celery('tasks', broker='redis://localhost', backend='redis://localhost')
 
 @app.task
 def add(x, y):
+    sleep(0.2)
     return x + y
 
-
+@trace
+def sleep(t):
+    time.sleep(t)
+    
