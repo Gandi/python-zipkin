359af21f1ae84c8b8c445fd63e135818f706adcc
Guillaume Gauvrit <guillaume@gandi.net>
2015-02-05 10:13:15 +0100

    Fix few ImportError

--- zipkin/config.py
+++ zipkin/config.py
@@ -1,31 +1,29 @@
 import socket
 import logging
-from pyramid.events import NewRequest
 
 from zipkin.models import Endpoint
 from zipkin.client import Client
-from zipkin_requests import init as request_init
-from zipkin_celery import zipkin_init as celery_init
 
 log = logging.getLogger(__name__)
 
 
-def configure(name, settings, requests=True, celery=True):
+def configure(name, settings, use_requests=True, use_celery=True):
     """Include the zipkin definitions"""
-
 
     ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
     endpoint = Endpoint(ip, 0, name)
     Client.configure(settings)
 
-    if requests:
+    if use_requests:
         try:
+            from zipkin_requests import init as request_init
             request_init()
         except ImportError:
             log.warn('package requests not installed')
 
-    if celery:
+    if use_celery:
         try:
+            from zipkin_celery import zipkin_init as celery_init
             celery_init(endpoint)
         except ImportError:
             log.warn('package celery not installed')
