0aad1b972633be702eec67e7c6e2dcf151a54f04
Romuald Brunet <romuald@gandi.net>
2015-02-27 14:49:44 +0100

    xmlrpclib bindings
    Added xmlrpclib monkeypatch to plug into zipkin

---
+++ zipkin/tests/bindings/test_xmlrpclib.py
@@ -0,0 +1,65 @@
+import sys
+
+try:
+    import unittest2 as unittest
+
+except ImportError:
+    import unittest
+
+import multiprocessing as mp
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+
+from mock import patch
+from time import time, sleep
+
+from xmlrpclib import ServerProxy
+
+import zipkin
+from zipkin.thread import local
+from zipkin.binding.xmlrpclib import bind as bind_zipkin
+
+from ..utils import DummyClient, dummy_log
+
+SERVERPORT = 18747
+
+
+class XMLRPClibAppTestCase(unittest.TestCase):
+    server = None
+
+    def setUp(self):
+        endpoint = zipkin.configure('My test xmlrpclib application',
+                                    {'zipkin.collector': 'www.remote.url',
+                                     'zipkin.service_name': 'my app'})
+
+        bind_zipkin(endpoint)
+
+        server = SimpleXMLRPCServer(('localhost', SERVERPORT), allow_none=True)
+        server.register_function(sleep)
+
+        self.server = mp.Process(target=server.serve_forever)
+        self.server.start()
+
+        DummyClient.reset()
+
+    def tearDown(self):
+        if self.server and self.server.is_alive():
+            self.server.terminate()
+
+    def test_sleep(self):
+        client = ServerProxy('http://localhost:%d/RPC2' % SERVERPORT,
+                             allow_none=True)
+
+        start = time()
+        client.sleep(0.1)
+        end = time()
+        delta = end - start
+
+        dummy_log(local().current)
+
+        self.assertGreater(delta, 0.101)
+
+        traces = DummyClient._client.messages
+        self.assertEquals(len(traces), 1,
+                          "There should be one trace for "
+                          "the request just processed")
