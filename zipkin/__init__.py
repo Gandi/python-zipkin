339ab70cd318677b1a5fe2cfefeb822132c72e4b
Arthur Gautier <baloo@gandi.net>
2016-07-05 15:24:18 +0000

    recv returns 0 on CLOSE_WAIT unlike -1 like recv(2) says
    Or it should throw a ENOTCONN. It doesn't either.
    
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- zipkin/__init__.py
+++ zipkin/__init__.py
@@ -5,6 +5,6 @@ from .config import configure
 from .thread import local  # XXX remove me from here
 
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 __all__ = ['trace', 'get_current_trace', 'stack_trace', 'configure', 'local']
