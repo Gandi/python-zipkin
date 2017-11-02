8651a32f0da25cbcb4bdc7e8b1108f44ef4c59e5
Arthur Gautier <baloo@gandi.net>
2017-11-02 19:17:17 +0000

    Try to map celery timings
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- zipkin/binding/celery/events.py
+++ zipkin/binding/celery/events.py
@@ -21,6 +21,7 @@ def task_send_handler(body, exchange, routing_key, headers, **kwargs):
     forwarded_trace = trace.child_noref("subservice")
     headers['X-B3-TraceId'] = hex_str(forwarded_trace.trace_id)
     headers['X-B3-SpanId'] = hex_str(forwarded_trace.span_id)
+    forwarded_trace.record(Annotation.client_send())
 
     if forwarded_trace.parent_span_id is not None:
         headers['X-B3-ParentSpanId'] = hex_str(forwarded_trace.parent_span_id)
@@ -43,6 +44,7 @@ def task_prerun_handler(task_id, task, **kwargs):
 def task_postrun_handler(task_id, task, **kwargs):
     trace = local().current
     trace.record(Annotation.server_send())
+    trace.record(Annotation.client_recv())
 
     log(trace)
     local().pop()
