e2790da73763184d7fc4c8eded9ca54d0a48809c
Arthur Gautier <baloo@gandi.net>
2019-09-10 08:43:29 +0000

    Release 0.5.6
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- zipkin/__init__.py
+++ zipkin/__init__.py
@@ -5,6 +5,6 @@ from .config import configure
 from .thread import local  # XXX remove me from here
 
 
-__version__ = '0.5.3'
+__version__ = '0.5.6'
 
 __all__ = ['trace', 'get_current_trace', 'stack_trace', 'configure', 'local']
