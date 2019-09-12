079f07cc55b936652b4f5c605408149f4f676eee
Arthur Gautier <baloo@gandi.net>
2019-09-12 07:06:01 +0000

    pyramid: filter out trace names
    do not record the path_qs in the trace name, zipkin ui does not handle
    great the cardinality.
    
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- zipkin/binding/pyramid/pyramidhook.py
+++ zipkin/binding/pyramid/pyramidhook.py
@@ -17,13 +17,23 @@ def wrap_request(endpoint):
         if getattr(request, 'trace', None):
             had_trace = True
 
-        trace = Trace(request.method + ' ' + request.matched_route.pattern,
+        trace_name = request.path_qs
+        if request.matched_route:
+            # we only get a matched route if we've gone through the router.
+            trace_name = request.matched_route.pattern
+
+        trace = Trace(request.method + ' ' + trace_name,
                       int_or_none(headers.get('X-B3-TraceId', None)),
                       int_or_none(headers.get('X-B3-SpanId', None)),
                       int_or_none(headers.get('X-B3-ParentSpanId', None)),
                       endpoint=endpoint)
+
         if 'X-B3-TraceId' not in headers:
             log.info('no trace info from request: %s', request.path_qs)
+
+        if request.matchdict:  # matchdict maybe none if no route is registered
+            for k, v in request.matchdict.items():
+                trace.record(Annotation.string('route.param.%s' % k, v))
 
         trace.record(Annotation.string('http.path', request.path_qs))
         log.info('new trace %r', trace.trace_id)
