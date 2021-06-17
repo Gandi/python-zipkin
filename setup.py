5535e452d9208f9a0607688be465fccdb080a2de
Guillaume Gauvrit <guillaume@gandi.net>
2021-06-17 17:24:59 +0200

    Run black on the setup.py

--- setup.py
+++ setup.py
@@ -3,61 +3,70 @@ import re
 import sys
 from setuptools import setup, find_packages
 
-name = 'zipkin'
+name = "zipkin"
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as f:
+with open(os.path.join(here, "README.rst")) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.rst')) as f:
+with open(os.path.join(here, "CHANGES.rst")) as f:
     CHANGES = f.read()
-with open(os.path.join(here, name, '__init__.py')) as version:
-    VERSION = re.compile(r".*__version__ = '(.*?)'",
-                         re.S).match(version.read()).group(1)
+with open(os.path.join(here, name, "__init__.py")) as version:
+    VERSION = (
+        re.compile(r".*__version__ = '(.*?)'", re.S).match(version.read()).group(1)
+    )
 
 requires = [
-    'thriftpy2',
-    'six',
-    ]
+    "thriftpy2",
+    "six",
+]
 
-tests_require = [
-    'pytest',
-    'mock'
-    ]
+tests_require = ["pytest", "mock"]
 
 if sys.version_info[:2] < (2, 7):
-    tests_require.append('unittest2')
+    tests_require.append("unittest2")
 
 extras_require = {
-    'pyramid': [
-        'pyramid',
+    "pyramid": [
+        "pyramid",
     ],
-    'celery': [
-        'celery >= 3.1',
+    "celery": [
+        "celery >= 3.1",
     ],
-    'requests': [
-        'requests',
+    "requests": [
+        "requests",
     ],
-    'flask': [
-        'flask', 'blinker',
+    "flask": [
+        "flask",
+        "blinker",
     ],
-    'dev': ['pyramid', 'celery', 'requests', 'flask', 'blinker', 'sphinx', 'sqlalchemy'],
-    'test': tests_require,
+    "dev": [
+        "blinker",
+        "celery",
+        "flask",
+        "pyramid",
+        "requests",
+        "sphinx",
+        "sqlalchemy",
+    ],
+    "test": tests_require,
 }
 
-setup(name='zk',
-      version=VERSION,
-      description='zipkin',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=['Programming Language :: Python',
-                   ],
-      author='Gandi',
-      author_email='feedback@gandi.net',
-      url='',
-      keywords='zipkin metrics trace',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      extras_require=extras_require,
-      tests_require=tests_require,
-      test_suite='{name}.tests'.format(name=name),
-      )
+setup(
+    name="zk",
+    version=VERSION,
+    description="zipkin",
+    long_description=README + "\n\n" + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author="Gandi",
+    author_email="feedback@gandi.net",
+    url="",
+    keywords="zipkin metrics trace",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=tests_require,
+    test_suite="{name}.tests".format(name=name),
+)
