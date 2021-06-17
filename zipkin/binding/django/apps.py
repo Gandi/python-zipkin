0d1930158331f309801e2b15382a392effdcdc49
Guillaume Gauvrit <guillaume@gandi.net>
2021-06-17 08:45:08 +0200

    Add binding for django

---
+++ zipkin/binding/django/apps.py
@@ -0,0 +1,60 @@
+"""
+Django App to trace call with zipkin.
+
+
+Usage:
+
+You need to add this in your django settings.
+
+::
+
+    ZIPKIN_SERVICE_NAME = "my-service-name"
+    ZIPKIN_COLLECTOR = "zipkin.localdomain"  # hostname of the zipkin server
+    ZIPKIN_ENV = "dev"  # Environment
+    ZIPKIN_BINDINGS = ["requests"]  # List of library to patch/attach
+
+    INSTALLED_APPS = [
+        ...,
+        "zipkin.binding.django",
+        ...
+    ]
+
+
+"""
+
+from django.conf import settings
+from django.apps import AppConfig
+from zipkin.config import configure
+
+
+def get_settings():
+    return {
+        "zipkin.collector": settings.ZIPKIN_COLLECTOR,
+        "zipkin.service_name": settings.ZIPKIN_SERVICE_NAME,
+        "zipkin.env": settings.ZIPKIN_ENV,
+    }
+
+
+class ZipkinConfig(AppConfig):
+    """
+    Configure zipkin from django settings.
+    """
+
+    name = "zipkin"
+    service_name = ""
+
+    def ready(self):
+
+        bindings = []  # settings.ZIPKIN_BINDINGS
+        appsettings = get_settings()
+        ZipkinConfig.service_name = "%s-%s" % (
+            settings.ZIPKIN_SERVICE_NAME,
+            settings.ZIPKIN_ENV,
+        )
+        configure(
+            appsettings["zipkin.service_name"],
+            appsettings,
+            use_celery="celery" in bindings,
+            use_requests="request" in bindings,
+            use_xmlrpclib="xmlrpclib" in bindings,
+        )
