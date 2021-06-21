56e770e244ae9499e247d0352facde8bb7a7c369
Guillaume Gauvrit <guillaume@gandi.net>
2021-06-21 17:40:16 +0200

    Create a context manager to trace par of function

--- zipkin/api.py
+++ zipkin/api.py
@@ -3,13 +3,43 @@ from functools import wraps
 from .models import Annotation
 from .thread import local
 
-__all__ = ['trace', 'get_current_trace']
+__all__ = ["trace", "get_current_trace"]
+
+
+class Trace(object):
+    def __init__(self, name):
+        self.name = name
+        self.recording = None
+        self.trace = None
+
+    def __enter__(self):
+
+        try:
+            self.trace = local().child(self.name)
+            annotation = Annotation.server_recv()
+            self.trace.record(annotation)
+            self.recording = True
+        except Exception:
+            self.recording = False
+
+    def __exit__(self, type, value, traceback):
+        if self.recording:
+            self.trace.record(Annotation.server_send())
+            local().pop()
+
+    def __call__(self, func):
+        @wraps(func)
+        def decorated(*args, **kwds):
+            with self:
+                return func(*args, **kwds)
+
+        return decorated
 
 
 def trace(name):
     """ A decorator that trace the decorated function """
 
-    if hasattr(name, '__call__'):
+    if hasattr(name, "__call__"):
         return trace(name.__name__)(name)
 
     def func_decorator(func):
@@ -31,6 +61,7 @@ def trace(name):
                     local().pop()
 
         return wrapper
+
     return func_decorator
 
 
